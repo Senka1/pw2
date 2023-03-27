05b3cf62fadf0dddac43331bb2b2e2829a026b16
Senka1 <40804722+Senka1@users.noreply.github.com>
2023-03-27 22:27:53 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Pw22
+{
+    class Number
+    {
+        private int n = 0;
+
+        public bool SetNumber(int number)
+        {
+            if (number == n + 1)
+            {
+                n = number;
+                return true;
+            }
+            else
+            {
+                n = 0;
+                return false;
+            }
+        }
+
+        public int GetExpectedNumber()
+        {
+            return n + 1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            int expectedNumber = 1;
+
+            while (true)
+            {
+                Console.Write($"ВВедите число {expectedNumber}: ");
+                int inputNumber = int.Parse(Console.ReadLine());
+
+                if (number.SetNumber(inputNumber))
+                {
+                    expectedNumber++;
+                }
+                else
+                {
+                    expectedNumber = 1;
+                    Console.WriteLine("Неправильно");
+                }
+
+                Console.WriteLine($"Ожидаемое число: {number.GetExpectedNumber()}");
+            }
+        }
+    }
+}
