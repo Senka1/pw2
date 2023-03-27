fc7be7e03236d5bcd2ea80ced5fb807f1698c5a4
Senka1 <40804722+Senka1@users.noreply.github.com>
2023-03-27 22:28:49 +0300

    Add files via upload

---
+++ 2.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace PW2
+{
+    class Number
+    {
+        private int n = 0;
+
+        public bool SetNumber(int number)
+        {
+            if (number == n + 1)
+            {
+                n = number;
+                return true;
+            }
+            else
+            {
+                n = 0;
+                return false;
+            }
+        }
+
+        public int GetExpectedNumber()
+        {
+            return n + 1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            int expectedNumber = 1;
+
+            while (true)
+            {
+                Console.Write($"Enter the number {expectedNumber}: ");
+                int inputNumber = int.Parse(Console.ReadLine());
+
+                if (number.SetNumber(inputNumber))
+                {
+                    expectedNumber++;
+                }
+                else
+                {
+                    expectedNumber = 1;
+                    Console.WriteLine("Wrong number entered. Starting over.");
+                }
+
+                Console.WriteLine($"Expected number: {number.GetExpectedNumber()}");
+            }
+        }
+    }
+}
---
+++ 2.3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace PW23
+{
+    class CaesarCipher
+    {
+        private readonly string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        private readonly int shift;
+
+        public CaesarCipher(int shift)
+        {
+            this.shift = shift % 32;
+        }
+
+        public string Encrypt(string message)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in message.ToLower())
+            {
+                int index = alphabet.IndexOf(c);
+
+                if (index == -1)
+                {
+                    sb.Append(c);
+                }
+                else
+                {
+                    int shiftedIndex = (index + shift) % 32;
+                    sb.Append(alphabet[shiftedIndex]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public string Decrypt(string message)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in message.ToLower())
+            {
+                int index = alphabet.IndexOf(c);
+
+                if (index == -1)
+                {
+                    sb.Append(c);
+                }
+                else
+                {
+                    int shiftedIndex = (index - shift + 32) % 32;
+                    sb.Append(alphabet[shiftedIndex]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            CaesarCipher cipher = new CaesarCipher(3);
+
+            Console.WriteLine("Введите текст:");
+            string originalMessage = Console.ReadLine();
+
+            string encryptedMessage = cipher.Encrypt(originalMessage);
+            string decryptedMessage = cipher.Decrypt(encryptedMessage);
+
+            Console.WriteLine($"Зашифроврованный вид: {encryptedMessage}");
+            Console.WriteLine($"Расшифроврованный вид: {decryptedMessage}");
+        }
+    }
+}
